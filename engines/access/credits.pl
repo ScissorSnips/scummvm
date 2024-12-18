9d78471ecfce99e4d63553ad31b6e579b2cc0d4e
username <14256056+ScissorSnips@users.noreply.github.com>
2024-12-18 01:47:20 -0600

    remove non-scumm engines

--- engines/access/credits.pl
+++
@@ -1,4 +0,0 @@
-begin_section("Access");
-	add_person("Arnaud Boutonn&eacute;", "Strangerke", "");
-	add_person("Paul Gilbert", "dreammaster", "");
-end_section();
--- engines/m4/credits.pl
+++
@@ -1,3 +0,0 @@
-begin_section("M4");
-	add_person("Paul Gilbert", "DreamMaster", "");
-end_section();
